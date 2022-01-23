811ef92068dae6774357d214b78a2ccd452739e1
ssukim <tntntn0930@gmail.com>
2022-01-23 16:28:05 +0900

    graphql practice

--- server.js
+++ server.js
@@ -2,16 +2,36 @@ import { ApolloServer, gql } from "apollo-server";
 
 // The GraphQL schema
 const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
   type Query {
     "A simple type for getting started!"
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => "js-log world~",
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2022 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
