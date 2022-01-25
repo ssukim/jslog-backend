b1228f2f7456183582505d41838067d4608f48cc
ssukim <tntntn0930@gmail.com>
2022-01-25 23:37:18 +0900

    user create, seeprofile add

--- movies/movies.queries.ts
+++
@@ -1,9 +0,0 @@
-import client from "../client";
-
-// A map of functions which return data for the schema.
-export default {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
-  },
-};
--- schema.ts
+++ schema.ts
@@ -2,7 +2,7 @@ import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 
-const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDef.ts`);
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.ts`);
 const loadedResolvers = loadFilesSync(
   `${__dirname}/**/*.{queries,mutations}.ts`
 );
