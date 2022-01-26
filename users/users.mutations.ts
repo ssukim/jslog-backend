6af8c83d4c01aee3329b6eba29fb603f90df5ecf
ssukim <tntntn0930@gmail.com>
2022-01-26 22:35:15 +0900

    edit profile 초기화

--- users/users.mutations.ts
+++ users/users.mutations.ts
@@ -1,5 +1,6 @@
 import client from "../client";
 import * as bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
@@ -39,6 +40,28 @@ export default {
       } catch (error) {
         return error;
       }
+    },
+    login: async (_, { username, password }) => {
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password.",
+        };
+      }
+
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
